28c592ac9eec30595c029c984581b70225c38f99
Clear492g <47822706+Clear492g@users.noreply.github.com>
2019-04-27 12:11:49 +0800

    V1.0

---
+++ TJPU-AutoLogin/MainWindow.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace TJPU_AutoLogin
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.小图标 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.还原ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.退出ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(26, 69);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(20, 20);
+            this.webBrowser1.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 1);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(326, 113);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 120);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 33);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "批量导入";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // 小图标
+            // 
+            this.小图标.ContextMenuStrip = this.contextMenuStrip1;
+            this.小图标.Icon = ((System.Drawing.Icon)(resources.GetObject("小图标.Icon")));
+            this.小图标.Text = "校园网自动登录";
+            this.小图标.Visible = true;
+            this.小图标.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.还原ToolStripMenuItem,
+            this.退出ToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(109, 52);
+            // 
+            // 还原ToolStripMenuItem
+            // 
+            this.还原ToolStripMenuItem.Name = "还原ToolStripMenuItem";
+            this.还原ToolStripMenuItem.Size = new System.Drawing.Size(108, 24);
+            this.还原ToolStripMenuItem.Text = "还原";
+            this.还原ToolStripMenuItem.Click += new System.EventHandler(this.还原ToolStripMenuItem_Click);
+            // 
+            // 退出ToolStripMenuItem
+            // 
+            this.退出ToolStripMenuItem.Name = "退出ToolStripMenuItem";
+            this.退出ToolStripMenuItem.Size = new System.Drawing.Size(108, 24);
+            this.退出ToolStripMenuItem.Text = "退出";
+            this.退出ToolStripMenuItem.Click += new System.EventHandler(this.退出ToolStripMenuItem_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(80, 170);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(159, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "By Clear 2019.04.21";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(226, 128);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(89, 19);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "开机自启";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(327, 205);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.webBrowser1);
+            this.Name = "MainWindow";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.F_Main_FormClosing);
+            this.Load += new System.EventHandler(this.MainWindow_Load);
+            this.SizeChanged += new System.EventHandler(this.F_Main_SizeChanged);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NotifyIcon 小图标;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 还原ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 退出ToolStripMenuItem;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ TJPU-AutoLogin/MainWindow.cs
@@ -0,0 +1,332 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Microsoft.Win32;
+
+
+
+namespace TJPU_AutoLogin
+{
+
+
+    public partial class MainWindow : Form
+    {
+        /***************************************************************************************/
+        public string web_url_TJPUNet = "http://172.23.4.5";
+        public string web_url_Baidu = "http://www.baidu.com";
+
+        bool IsAutoRun;
+
+        public string[] name = new string[1000];
+        public string[] pass = new string[1000];
+
+        int txtRows;
+        int IDnum;
+        int NowID;
+
+        /***************************************************************************************/
+
+        public MainWindow()//内部代码自动生成
+        {
+            InitializeComponent();
+          
+            
+            StartPosition = FormStartPosition.CenterScreen;
+        }
+
+        private void MainWindow_Load(object sender, EventArgs e)
+        {
+            this.Text = "AutoLogin";
+            pictureBox1.Image = Image.FromFile(Application.StartupPath + "\\tjpu.jpg");
+
+            try
+            {
+                RegistryKey loca_chek = Registry.CurrentUser;
+                RegistryKey run_Check = loca_chek.CreateSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run");
+
+                if (run_Check.GetValue("NetAutoLogin").ToString().ToLower() != "false")
+                {
+                    checkBox1.Checked = true;
+                }
+                else
+                {
+                    checkBox1.Checked = false;
+                }
+            }
+
+            catch { }
+            
+
+            webBrowser1.Visible = false;
+            webBrowser1.ScriptErrorsSuppressed = true;
+            webBrowser1.Navigate(web_url_Baidu);
+
+
+
+
+            string filepath1 = Application.StartupPath + "\\ID.txt";
+
+            txtRows = GetRows(filepath1);
+            IDnum = txtRows - 2;
+
+            NowID = 0;
+
+            IDLoad(filepath1, IDnum);
+
+            timer1.Interval = 5000;
+            timer1.Enabled = true;
+
+            timer2.Interval = 2000;
+            timer2.Enabled = false;
+
+            timer3.Interval = 1000;
+            timer3.Enabled = false;
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)//主，定时刷新Baidu 
+        {
+            timer1.Enabled = false;
+            timer2.Enabled = false;
+            timer3.Enabled = false;
+
+            webBrowser1.Refresh();
+            timer2.Enabled = true;
+           
+        }
+        private void timer2_Tick(object sender, EventArgs e)//等待主刷新结束 查看Baidu是否加载
+        {
+
+            timer1.Enabled = false;
+            timer2.Enabled = false;
+            timer3.Enabled = false;
+
+
+            if (webBrowser1.DocumentTitle != "百度一下，你就知道")
+            {
+                //MessageBox.Show(webBrowser1.DocumentTitle);
+                webBrowser1.Navigate(web_url_TJPUNet);
+                timer3.Enabled = true;
+            }
+            else timer1.Enabled = true;
+
+        }
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            timer2.Enabled = false;
+            timer3.Enabled = false;
+
+            if ((NowID >=IDnum)||(NowID>=1000)) NowID = 0;
+
+            TryToConnect(name[NowID], pass[NowID]);
+
+            NowID++;
+
+            webBrowser1.Navigate(web_url_Baidu);
+
+            timer2.Enabled = true;
+
+        }
+
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)//角标
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+                //还原窗体显示    
+                WindowState = FormWindowState.Normal;
+                //激活窗体并给予它焦点
+                this.Activate();
+                //任务栏区显示图标
+                this.ShowInTaskbar = true;
+                //托盘区图标隐藏
+                小图标.Visible = false;
+            }
+
+        }
+
+        private void F_Main_SizeChanged(object sender, EventArgs e)//角标
+        {
+            //判断是否选择的是最小化按钮
+            if (WindowState == FormWindowState.Minimized)
+            {
+                //隐藏任务栏区图标
+                this.ShowInTaskbar = false;
+                //图标显示在托盘区
+                小图标.Visible = true;
+            }
+        }
+
+        private void F_Main_FormClosing(object sender, FormClosingEventArgs e)//角标
+        {
+            if (MessageBox.Show("是否确认退出自动登录程序？", "退出", MessageBoxButtons.OKCancel, MessageBoxIcon.Question) == DialogResult.OK)
+            {
+                // 关闭所有的线程
+                this.Dispose();
+                this.Close();
+            }
+            else
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void 还原ToolStripMenuItem_Click(object sender, EventArgs e)//角标
+        {
+            WindowState = FormWindowState.Normal;
+        }
+
+        private void 退出ToolStripMenuItem_Click(object sender, EventArgs e)//角标
+        {
+            if (MessageBox.Show("是否确认退出自动登录程序？", "退出", MessageBoxButtons.OKCancel, MessageBoxIcon.Question) == DialogResult.OK)
+            {
+                // 关闭所有的线程
+                this.Dispose();
+                this.Close();
+            }
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)//批量添加按钮
+        {
+            if (MessageBox.Show("请在确认后弹出的文本中导入,之后重启程序。", "了解", MessageBoxButtons.OKCancel, MessageBoxIcon.Question) == DialogResult.OK)
+            {
+                System.Diagnostics.Process pro = new System.Diagnostics.Process();
+                pro.StartInfo.FileName = Application.StartupPath + "\\ID.txt";
+                pro.Start();//关键
+
+            }
+
+        }
+
+        private int GetRows(string FilePath)//获取ID.txt行数
+        {
+            int lines = 0;  //用来统计txt行数
+            FileStream fs = new FileStream(FilePath, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            StreamReader sr = new StreamReader(fs);
+            while (sr.ReadLine() != null)
+            {
+                lines++;
+            }
+
+            fs.Close();
+            sr.Close();
+
+            return lines;
+        }
+
+        private void IDLoad(string FilePath,int num)//从ID.txt中导入账号
+        {
+
+            FileStream MyInFile1 = null;
+
+            MyInFile1 = new FileStream(FilePath, FileMode.Open, FileAccess.Read);
+            StreamReader myfile = new StreamReader(MyInFile1);
+            string str = "";
+            str = myfile.ReadLine();
+            str = myfile.ReadLine();
+            str = myfile.ReadLine();
+            string[] aa = str.Split(',');
+
+            for (int i = 0; i < num; i++)
+            {
+                try
+                {
+                    name[i] = aa[0];
+                    pass[i] = aa[1];
+
+                    str = myfile.ReadLine();
+                    aa = str.Split(',');
+                }
+                catch { }
+
+            }
+
+            MyInFile1.Close();
+            myfile.Close();
+        }
+
+        private void TryToConnect(string NewID, string NewPass)//尝试登录
+        {
+            try
+            {
+                //MessageBox.Show("正在尝试ID:" + NewID + "\n" + "PASS:" + NewPass);
+                HtmlElement tbUserid = webBrowser1.Document.All["DDDDD"];
+                HtmlElement tbPasswd = webBrowser1.Document.All["upass"];
+
+                if (!(tbUserid == null || tbPasswd == null))
+                {
+                    tbUserid.SetAttribute("value", NewID);
+                    tbPasswd.SetAttribute("value", NewPass);
+
+                }
+
+                string str = null;
+
+                for (int i = 0; i < webBrowser1.Document.All.Count; i++)
+                {
+
+                    str = webBrowser1.Document.All[i].GetAttribute("name");
+
+                    if (str != "")
+                    {
+
+                        if (str != null && str.Trim() == "0MKKey")
+                        {
+
+                            HtmlElement he = webBrowser1.Document.All[i];
+
+                            he.InvokeMember("click");
+
+                            return;
+
+                        }
+                    }
+
+                }
+            }
+
+            catch { }
+
+            
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+   
+            if (checkBox1.Checked == true) IsAutoRun = true;
+            else IsAutoRun = false;
+            try
+            {
+                //获取程序执行路径..
+                string starupPath = Application.ExecutablePath;
+                //class Micosoft.Win32.RegistryKey. 表示Window注册表中项级节点,此类是注册表装.
+                RegistryKey loca = Registry.CurrentUser;
+                RegistryKey run = loca.CreateSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Run");
+
+                //SetValue:存储值的名称
+                if (IsAutoRun == false) run.SetValue("NetAutoLogin", false);//取消开机运行
+                else run.SetValue("NetAutoLogin", starupPath);//设置开机运行
+                loca.Close();
+
+            }
+            catch
+            { }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("BUG提交：QQ:2015531274");
+        }
+    }
+}
